999ff1cd594008f453b924766cd8965982fc0938
Siyandan <Siyanda.Ngcobo@derivco.co.za>
2020-01-03 13:09:03 +0200

    working template that gets data from api using fake data api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { DataComponent } from './data/data.component';
 import { PhotosComponent } from './photos/photos.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { NgModule } from '@angular/core';
@@ -8,7 +7,6 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', component: CategoriesComponent },
   { path: 'photos/:albumId', component: PhotosComponent },
-  { path: 'data', component: DataComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,43 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { PhotosComponent } from './photos/photos.component';
-import { DataComponent } from './data/data.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { NgMatSearchBarModule } from 'ng-mat-search-bar';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriesComponent,
-    PhotosComponent,
-    DataComponent
+    PhotosComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
+    NgMatSearchBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
