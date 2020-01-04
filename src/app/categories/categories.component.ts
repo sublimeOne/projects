1c2e232c120a2013cf2eb94d19b95aeffd5084bb
Siyandan <Siyanda.Ngcobo@derivco.co.za>
2020-01-05 01:51:47 +0200

    update

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -28,7 +28,6 @@ export class CategoriesComponent implements OnInit {
   }
 
   loadAlbumTitles() {
-    debugger;
     this.spinnerService.show();
     this.albums = this.api.getAlbums().subscribe(res => {
       this.albums = res;
